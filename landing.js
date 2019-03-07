0b4427f3a429bb5cfdb1e72f298b2597ee74621a
Christian Gomez <cog3@nji.edu>
2019-03-06 22:49:53 -0500

    Image Intervals & Container

--- landing.js
+++ landing.js
@@ -1,23 +1,21 @@
-var rem;
-var currentImage;
-
-
-var images[] = new Array();
-images[0] = new Image();
-images[0].src = 'images/bitcoin.jpg';
-images[1] = new Image();
-images[1].src = 'images/ethereum.jpg';
-images[2] = new Image();
-images[2].src = 'images/Ripple.jpg';
-
-
-function removeActive(){
-	rem = document.getElementTagName("div");
-	rem.classList.remove("active");
-	if (currentImage) {}
-	//setInterval(popImage, 3000);
-}
-
-function popImage(){
-
+var current;
+var nextImage;
+var images = ["img1","img2","img3"];
+var count = 0; 
+var currentSlideIs = images[count];
+function active(){
+	setInterval(function(){
+		current = document.getElementById(images[count]);
+		current.className = "";
+		console.log(current.id);
+			
+		count++;
+		
+		if(count > 2){
+			count = 0;
+		}
+		
+		nextImage = document.getElementById(images[count]);
+		nextImage.className = "active";
+}, 3000);
 }
